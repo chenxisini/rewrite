43718d1498531f9ddf9c6631780353a4f8dc56c2
chenxisini <80562183+chenxisini@users.noreply.github.com>
2022-11-12 09:42:29 +0800

    Update coolapk.js

--- coolapk.js
+++ coolapk.js
@@ -7,7 +7,7 @@ author      kk pp
 
 
 [rewrite_local]
-^https?:\/\/api.coolapk.com\/v6\/(feed\/(replyList|detail)|main\/indexV8|dataList) url script-response-body https://github.com/chenxisini/rewrite/edit/main/coolapk.js
+^https?:\/\/api.coolapk.com\/v6\/(feed\/(replyList|detail)|main\/indexV8|dataList) url script-response-body https://raw.githubusercontent.com/chenxisini/rewrite/main/coolapk.js
 
 [mitm]
 hostname = api.coolapk.com
