dad2e67e4ee29d5d7503f83fc9648804f749db7f
chenxisini <80562183+chenxisini@users.noreply.github.com>
2022-11-12 10:00:52 +0800

    Create listen.js

---
+++ listen.js
@@ -0,0 +1,14 @@
+
+> 应用名称：趣听音乐 by@ddgksf2013
+
+            
+[rewrite_local]
+
+# ～ 趣听音乐☆自动激活APP（2022-10-26）@ddgksf2013
+^https?:\/\/api\.bspapp\.com\/client$ url script-response-body https://codeberg.org/ddgksf2013/Cuttlefish/raw/branch/master/Crack/qtmusic.js
+
+[mitm]
+
+hostname=api.bspapp.com
+
+***********************************/
